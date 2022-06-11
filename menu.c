1d639625f194be97bfc4970878403486cd6d8a6b
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-06-11 23:52:39 +0200

    Ukr language formatting

--- menu.c
+++ menu.c
@@ -1,4 +1,4 @@
-//
+//меню
 #include "Header.h"
 #include "Time.h"
 #include "Sort.h"
@@ -10,8 +10,8 @@ void menu()
 int p, m, n;
 int choise, flag = 1;
 
-printf("     -  1\n");
-printf("    -  2\n");
+printf("Перевірка роботи всіх алгоритмів  -  1\n");
+printf("Тестування часу роботи алгоритмів -  2\n");
 scanf("%d", &choise);
 
 switch(choise)
@@ -19,32 +19,32 @@ switch(choise)
 
    case 1:
 
-   	printf("  \n");
+   	printf("Введіть кількість перерізів\n");
    	scanf("%d", &p);
 
 	m = 4;
 	n = 4;
 
-	printf("\n \n");
+	printf("\nПочатковий масив\n");
 	input(p, m, n, 2);
 	output(p, m, n);
-	printf("  1\n");
+	printf("Сортування алгоритмом 1\n");
 	sort_1(p, m, n);
 	output(p, m, n);
 
 
-	printf("\n \n");
+	printf("\nПочатковий масив\n");
 	input(p, m, n, 2);
 	output(p, m, n);
-	printf("  2\n");
+	printf("Сортування алгоритмом 2\n");
 	sort_2(p, m, n);
 	output(p, m, n);
 
 
-	printf("\n \n");
+	printf("\nПочатковий масив\n");
 	input(p, m, n, 2);
 	output(p, m, n);
-	printf("  3\n");
+	printf("Сортування алгоритмом 3\n");
 	sort_3(p, m, n);
 	output(p, m, n);
 
@@ -53,9 +53,9 @@ switch(choise)
 
 	case 2:
 
-	printf("     \n");
-	printf("1 - \n");
-	printf("2 - \n");
+	printf("Запуск тестування часу всіх алгоритмів сортування\n");
+	printf("1 - Всі\n");
+	printf("2 - Один\n");
 	scanf("%d", &choise);
 
 	switch(choise)
@@ -64,20 +64,20 @@ switch(choise)
 
 	    case 1:
 
-	    printf("\n P\n");
+	    printf("\nВведіть P\n");
 	    scanf("%d", &p);
-	    printf("\n M\n");
+	    printf("\nВведіть M\n");
 	    scanf("%d", &m);
-	    printf("\n N\n");
+	    printf("\nВведіть N\n");
 	    scanf("%d", &n);
 
 		printf("\n______________________________________________________________________________________________________________\n");
-		printf("                                  || || | | \n");
-		printf(" 1 |      6      | |%10.0f   | |%10.0f | |%10.0f        |\n", (double)time_sort_1(p, m, n, 1), (double)time_sort_1(p, m, n, 2), (double)time_sort_1(p, m, n, 3));
+		printf("                                  |Впорядкований| |Випадковий| |Обернено впорядкований| \n");
+		printf("Сорт 1 |    Прямий вибір 6      | |%10.0f   | |%10.0f | |%10.0f        |\n", (double)time_sort_1(p, m, n, 1), (double)time_sort_1(p, m, n, 2), (double)time_sort_1(p, m, n, 3));
 		printf("________________________________________________________________________________________________________________\n");
-		printf(" 2 |      1      | |%10.0f   | |%10.0f | |%10.0f        |\n", (double)time_sort_2(p, m, n, 1), (double)time_sort_2(p, m, n, 2), (double)time_sort_2(p, m, n, 3));
+		printf("Сорт 2 |    Прямий обмін 1      | |%10.0f   | |%10.0f | |%10.0f        |\n", (double)time_sort_2(p, m, n, 1), (double)time_sort_2(p, m, n, 2), (double)time_sort_2(p, m, n, 3));
 		printf("________________________________________________________________________________________________________________\n");
-		printf(" 3 |? ( 4 - )| |%10.0f   | |%10.0f | |%10.0f        |\n", (double)time_sort_3(p, m, n, 1), (double)time_sort_3(p, m, n, 2), (double)time_sort_3(p, m, n, 3));
+		printf("Сорт 3 |Гібрид (вибір 4 - обмін)| |%10.0f   | |%10.0f | |%10.0f        |\n", (double)time_sort_3(p, m, n, 1), (double)time_sort_3(p, m, n, 2), (double)time_sort_3(p, m, n, 3));
 
 		break;
 
@@ -85,51 +85,51 @@ switch(choise)
         case 2:
 
 
-        printf("     - \n    1 - \n    2 - \n    3 -   \n");
+        printf("    Сортувати - \n    1 - Впорядкований\n    2 - Випадковий\n    3 -  Обернено впорядкований\n");
         scanf("%d", &mode_arr);
-        printf(" \n");
-        printf("1 -   (6)\n");
-        printf("2 -   (6)\n");
-        printf("3 -  (4) -  (6)\n");
+        printf("Метод сортування\n");
+        printf("1 - Прямий вибір (6)\n");
+        printf("2 - Прямий обмін (6)\n");
+        printf("3 - Вибір (4) - Обмін (6)\n");
         scanf("%d", &mode_sort);
 
         switch(mode_sort)
         {
             case 1:
-            printf("  \n");
+            printf("Введіть розміри масиву\n");
             printf("P = \n");
             scanf("%d", &p);
             printf("M = \n");
             scanf("%d", &m);
             printf("N = \n");
             scanf("%d", &n);
-            printf("  = %f", (double)time_sort_1(p, m, n, mode_arr));
+            printf("Час сортування = %f", (double)time_sort_1(p, m, n, mode_arr));
 
             break;
 
 
             case 2:
-            printf("  \n");
+            printf("Введыть розміри масиву\n");
             printf("P = \n");
             scanf("%d", &p);
             printf("M = \n");
             scanf("%d", &m);
             printf("N = \n");
             scanf("%d", &n);
-            printf("  = %f", (double)time_sort_2(p, m, n, mode_arr));
+            printf("Час сортування = %f", (double)time_sort_2(p, m, n, mode_arr));
 
             break;
 
 
             case 3:
-            printf("  \n");
+            printf("Введыть розміри масиву\n");
             printf("P = \n");
             scanf("%d", &p);
             printf("M = \n");
             scanf("%d", &m);
             printf("N = \n");
             scanf("%d", &n);
-            printf("  = %f", (double)time_sort_3(p, m, n, mode_arr));
+            printf("Час сортування = %f", (double)time_sort_3(p, m, n, mode_arr));
 
             break;
 
@@ -150,14 +150,14 @@ switch(choise)
 
 
 }
-printf("\n?\n 1 - \n2 -   \n");
+printf("\nМеню?\n 1 - Так\n2 - Закрити програму \n");
 scanf("%d", &flag);
 
 while(flag == 1)
 {
 system("cls");
 menu();
-printf("\n\n?\n 1 - \n2 -   \n");
+printf("\n\nМеню?\n 1 - Так\n2 - Закрити програму \n");
 scanf("%d", &flag);
 
 }
